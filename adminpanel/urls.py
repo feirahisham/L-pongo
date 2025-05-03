2cb33c211e6cb5803b196b505d64ac065f75d23c
= <=>
2025-05-03 23:31:17 +0800

    first day learning

---
+++ adminpanel/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('products/', views.product_list, name='product_list'),
+    path('products/add/', views.add_product, name='add_product'),
+    path('products/edit/<int:pk>/', views.edit_product, name='edit_product'),
+    path('products/delete/<int:pk>/', views.delete_product, name='delete_product'),
+]
--- mol01/settings.py
+++ mol01/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'adminpanel',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mol01.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'adminpanel' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
